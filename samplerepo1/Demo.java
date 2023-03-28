4caf181c7bf2b56c16ac77166fdfe4adf9b44449
Vijay <vijay.kumar@nebulogic.com>
2023-03-28 17:19:12 +0530

    Java and txt files added

--- samplerepo1/Demo.java
+++ samplerepo1/Demo.java
@@ -1,1 +1,6 @@
-rec1
+Class Demo {
+       Public  static void main(String[] args){
+       System.out.println("This is a demo program.");
+       System.out println("Git is very easy to operate");
+       } 
+}
